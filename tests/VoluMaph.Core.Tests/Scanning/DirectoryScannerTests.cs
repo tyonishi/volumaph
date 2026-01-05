fd9ad970b291d618c43f38f1e8c9006023244155
TSUYOSHI ONISHI <tyonishi@capricornus.biz>
2026-01-05 21:05:45 +0900

    test: wait for progress callback in ScanAsync_ShouldReportProgress

--- tests/VoluMaph.Core.Tests/Scanning/DirectoryScannerTests.cs
+++ tests/VoluMaph.Core.Tests/Scanning/DirectoryScannerTests.cs
@@ -63,12 +63,14 @@ public class DirectoryScannerTests : IDisposableTest
         CreateDirectory("subfolder");
         CreateFile("subfolder/file3.txt", 300);
 
-        var progressCount = 0;
-        var progress = new Progress<ScanProgress>(_ => progressCount++);
+        var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+        var progress = new Progress<ScanProgress>(_ => tcs.TrySetResult(true));
 
         await _scanner.ScanAsync(TestDirectory, progress);
 
-        Assert.True(progressCount > 0);
+        // Wait up to 2 seconds for the progress callback to be delivered.
+        var completed = await Task.WhenAny(tcs.Task, Task.Delay(2000));
+        Assert.True(completed == tcs.Task, "Progress callback was not invoked within timeout.");
     }
 
     [Fact]
